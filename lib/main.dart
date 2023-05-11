04dd34fe086a83caf346aad2ab5f5ba647b48a85
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-05-11 16:57:14 +0300

    refactor the code

--- lib/main.dart
+++ lib/main.dart
@@ -11,72 +11,81 @@ class tokuApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Scaffold(
-        backgroundColor: Color(0xfffef6db),
-        appBar: AppBar(
-          title: Text('Toku'),
-          backgroundColor: Color(0xff355070),
-        ),
-        body: Column(
-          children: [
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              height: 65,
-              width: double.infinity,
-              color: Color(0xff6D597A),
-              child: Text(
-                'Numbers',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffef6db),
+      appBar: AppBar(
+        title: Text('Toku'),
+        backgroundColor: Color(0xff355070),
+      ),
+      body: Column(
+        children: [
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: Color(0xff6D597A),
+            child: Text(
+              'Numbers',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              height: 65,
-              width: double.infinity,
-              color: Color(0xffB56576),
-              child: Text(
-                'FamilyMembers',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: Color(0xffB56576),
+            child: Text(
+              'FamilyMembers',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              height: 65,
-              width: double.infinity,
-              color: Color(0xffE56B6F),
-              child: Text(
-                'Colors',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: Color(0xffE56B6F),
+            child: Text(
+              'Colors',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
               ),
             ),
-            Container(
-              padding: EdgeInsets.only(left: 24),
-              alignment: Alignment.centerLeft,
-              height: 65,
-              width: double.infinity,
-              color: Color(0xffEAAC8B),
-              child: Text(
-                'Phrases',
-                style: TextStyle(
-                  fontSize: 20,
-                  color: Colors.white,
-                ),
+          ),
+          Container(
+            padding: EdgeInsets.only(left: 24),
+            alignment: Alignment.centerLeft,
+            height: 65,
+            width: double.infinity,
+            color: Color(0xffEAAC8B),
+            child: Text(
+              'Phrases',
+              style: TextStyle(
+                fontSize: 20,
+                color: Colors.white,
               ),
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
