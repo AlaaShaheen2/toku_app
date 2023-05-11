5ce3187f269893071e9ea4b2f4967362c4304b69
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-05-11 17:20:47 +0300

    refactor the code

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:toku_app/screens/home_page.dart';
 
 void main() {
   runApp(const tokuApp());
@@ -12,81 +13,6 @@ class tokuApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: HomePage(),
-    );
-  }
-}
-
-class HomePage extends StatelessWidget {
-  const HomePage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color(0xfffef6db),
-      appBar: AppBar(
-        title: Text('Toku'),
-        backgroundColor: Color(0xff355070),
-      ),
-      body: Column(
-        children: [
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            height: 65,
-            width: double.infinity,
-            color: Color(0xff6D597A),
-            child: Text(
-              'Numbers',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            height: 65,
-            width: double.infinity,
-            color: Color(0xffB56576),
-            child: Text(
-              'FamilyMembers',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            height: 65,
-            width: double.infinity,
-            color: Color(0xffE56B6F),
-            child: Text(
-              'Colors',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-          Container(
-            padding: EdgeInsets.only(left: 24),
-            alignment: Alignment.centerLeft,
-            height: 65,
-            width: double.infinity,
-            color: Color(0xffEAAC8B),
-            child: Text(
-              'Phrases',
-              style: TextStyle(
-                fontSize: 20,
-                color: Colors.white,
-              ),
-            ),
-          ),
-        ],
-      ),
     );
   }
 }
---
+++ lib/screens/home_page.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+
+import '../components/category_item.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xfffef6db),
+      appBar: AppBar(
+        title: Text('Toku'),
+        backgroundColor: Color(0xff355070),
+      ),
+      body: Column(
+        children: [
+          Category(
+            text: 'Number',
+            color: Color(0xff6D597A),
+          ),
+          Category(
+            text: 'FamilyMembers',
+            color: Color(0xffB56576),
+          ),
+          Category(
+            text: 'Colors',
+            color: Color(0xffE56B6F),
+          ),
+          Category(
+            text: 'Phrases',
+            color: Color(0xffEAAC8B),
+          ),
+        ],
+      ),
+    );
+  }
+}
