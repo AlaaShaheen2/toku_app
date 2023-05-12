3606f8564d372cc9622ed74906c713531a220d61
AlaaShaheen2 <alaa.m.shaheen22@gmail.com>
2023-05-12 13:22:13 +0300

    list view builder widget

--- lib/components/item.dart
+++ lib/components/item.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../models/number.dart';
 
-
 class Item extends StatelessWidget {
   const Item({super.key, required this.number});
   final Number number;
@@ -10,46 +9,46 @@ class Item extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-            height: 100,
-            color: Color(0xffef9235),
-            child: Row(
+      height: 100,
+      color: Color(0xff6D597A),
+      child: Row(
+        children: [
+          Container(color: Color(0xfffff6dc), child: Image.asset(number.image)),
+          Padding(
+            padding: const EdgeInsets.only(left: 18),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Container(color: Color(0xfffff6dc), child: Image.asset(number.image)),
-                Padding(
-                  padding: const EdgeInsets.only(left: 18),
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        number.jbName,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                      ),
-                      Text(
-                        number.enName,
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 18,
-                        ),
-                      ),
-                    ],
+                Text(
+                  number.jbName,
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
                   ),
                 ),
-                Spacer(
-                  flex: 1,
-                ),
-                Padding(
-                  padding: const EdgeInsets.only(right: 18),
-                  child: Icon(
-                    Icons.play_arrow,
+                Text(
+                  number.enName,
+                  style: TextStyle(
                     color: Colors.white,
-                    size: 30,
+                    fontSize: 18,
                   ),
                 ),
               ],
             ),
-          );
+          ),
+          Spacer(
+            flex: 1,
+          ),
+          IconButton(
+            onPressed: () {},
+            icon: Icon(
+              Icons.play_arrow,
+              color: Colors.white,
+              size: 30,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
--- lib/screens/numbers_page.dart
+++ lib/screens/numbers_page.dart
@@ -5,10 +5,55 @@ import '../models/number.dart';
 
 class NumbersPage extends StatelessWidget {
   const NumbersPage({super.key});
-  final Number one = const Number(
-      image: 'assets/images/numbers/number_one.png',
-      jbName: 'ichi',
-      enName: 'one');
+  final List<Number> numbers = const [
+    Number(
+        image: 'assets/images/numbers/number_one.png',
+        jbName: 'ichi',
+        enName: 'one'),
+    Number(
+        image: 'assets/images/numbers/number_two.png',
+        jbName: 'Ni',
+        enName: 'two'),
+    Number(
+      image: 'assets/images/numbers/number_three.png',
+      jbName: 'San',
+      enName: 'three',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_four.png',
+      jbName: 'Shi',
+      enName: 'four',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_five.png',
+      jbName: 'Go',
+      enName: 'five',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_six.png',
+      jbName: 'Roku',
+      enName: 'six',
+    ),
+    Number(
+        image: 'assets/images/numbers/number_seven.png',
+        jbName: 'Sebun',
+        enName: 'seven'),
+    Number(
+      image: 'assets/images/numbers/number_eight.png',
+      jbName: 'hachi',
+      enName: 'eight',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_nine.png',
+      jbName: 'Kyū',
+      enName: 'nine',
+    ),
+    Number(
+      image: 'assets/images/numbers/number_ten.png',
+      jbName: 'Jū',
+      enName: 'ten',
+    ),
+  ];
 
   @override
   Widget build(BuildContext context) {
@@ -17,13 +62,23 @@ class NumbersPage extends StatelessWidget {
         title: Text('Numbers'),
         backgroundColor: Color(0xff355070),
       ),
-      body: Column(
-        children: [
-          Item(number: one),
-          Item(number: one),
-          Item(number: one),
-        ],
-      ),
+      body: ListView.builder(
+          itemCount: numbers.length,
+          itemBuilder: (context, index) {
+            return Item(number: numbers[index]);
+          }),
     );
   }
+
+  // List<Widget> getList(List<Number> numbers) {
+  //   List<Widget> itemList = [];
+  //   for (int i = 0; i < numbers.length; i++) {
+  //     itemList.add(
+  //       Item(
+  //         number: numbers[i],
+  //       ),
+  //     );
+  //   }
+  //   return itemList;
+  // }
 }
